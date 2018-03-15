08103508e962891b6bfef58e64a2527688b8beab
workdestiny <workdestiny@hotmail.com>
2018-03-15 14:15:39 +0700

    update ReFormatTime FormatTime

--- convbox.go
+++ convbox.go
@@ -2,6 +2,7 @@ package convbox
 
 import (
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -57,4 +58,110 @@ func GetMonth(t time.Time) string {
 func GetDay(t time.Time) string {
 	date := t.Format(timeFormat)
 	return date[8:10]
+}
+
+func ReFormatTime(t int64) string {
+
+	var sTime string
+	now := time.Now().Unix()
+
+	duration := now - t
+
+	if duration < 60 {
+		sDuration := strconv.FormatInt(duration, 10)
+		sTime = sDuration + " วินาทีที่แล้ว"
+	} else if duration < 3600 {
+		mDuration := strconv.FormatInt(duration/60, 10)
+		sTime = mDuration + " นาที"
+	} else if duration < 86400 {
+		hDuration := strconv.FormatInt(duration/3600, 10)
+		sTime = hDuration + " ชั่วโมง"
+	}
+	if duration >= 86400 {
+		formatTime := time.Unix(t, 0).Format("2006-01-02 15:04:05")
+		values := strings.Split(formatTime, "-")
+		if values[1] != "" {
+
+			var mouth string
+			switch values[1] {
+			case "01":
+				mouth = "มกราคม"
+			case "02":
+				mouth = "กุมภาพันธ์"
+			case "03":
+				mouth = "มีนาคม"
+			case "04":
+				mouth = "เมษายน"
+			case "05":
+				mouth = "พฤษภาคม"
+			case "06":
+				mouth = "มิถุนายน"
+			case "07":
+				mouth = "กรกฎาคม"
+			case "08":
+				mouth = "สิงหาคม"
+			case "09":
+				mouth = "กันยายน"
+			case "10":
+				mouth = "ตุลาคม"
+			case "11":
+				mouth = "พฤศจิกายน"
+			case "12":
+				mouth = "ธันวาคม"
+			}
+
+			dayTime := strings.Split(values[2], " ")
+			sTime = dayTime[0] + " " + mouth + " " + values[0] + " " + dayTime[1]
+
+		} else {
+			sTime = formatTime
+		}
+
+	}
+
+	return sTime
+}
+
+func FormatTime(t int64) string {
+
+	var sTime string
+
+	formatTime := time.Unix(t, 0).Format("2006-01-02 15:04:05")
+	values := strings.Split(formatTime, "-")
+	if values[1] != "" {
+
+		var mouth string
+		switch values[1] {
+		case "01":
+			mouth = "มกราคม"
+		case "02":
+			mouth = "กุมภาพันธ์"
+		case "03":
+			mouth = "มีนาคม"
+		case "04":
+			mouth = "เมษายน"
+		case "05":
+			mouth = "พฤษภาคม"
+		case "06":
+			mouth = "มิถุนายน"
+		case "07":
+			mouth = "กรกฎาคม"
+		case "08":
+			mouth = "สิงหาคม"
+		case "09":
+			mouth = "กันยายน"
+		case "10":
+			mouth = "ตุลาคม"
+		case "11":
+			mouth = "พฤศจิกายน"
+		case "12":
+			mouth = "ธันวาคม"
+		}
+
+		dayTime := strings.Split(values[2], " ")
+		sTime = dayTime[0] + " " + mouth + " " + values[0]
+
+	}
+
+	return sTime
 }
